9508015433bc5b644cd2ab7bb6c3484b17c36f1c
durgeshbijewar <137867028+durgeshbijewar@users.noreply.github.com>
2023-11-30 01:58:06 -0500

    Add files via upload

---
+++ metacoin.sol
@@ -0,0 +1,20 @@
+//what is truffle
+//ans=truffle suit is development enviroment based on etherioum blockchain. used to develop
+//Dapps(decentralized application)
+//what is ganache?
+//Ganache is a personal ethereum blockchin used to testsmart conract where  you can disploy contract develop application,run test and perform other taskwithout any cost
+
+//demontrate intallation process of truffle suit
+//ans= nmp install truffle -g
+$ truffle version
+Truffle v5.11.5 (core: 5.11.5)
+Ganache v7.9.1
+Solidity v0.5.16 (solc-js)
+Node v18.18.2
+Web3.js v1.10.0
+//creating project
+//contract/Metacoin.sol
+//migration/1_deploy_contract.js
+//test/TestMetacoin.sol
+//test/metacoin.js
+//truffle-config.js
